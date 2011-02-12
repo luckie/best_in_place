92c573ca0fa4fd56db5592efac12f6f7ff150b86
Bernat Farrero <bernat@itnig.net>
2011-02-12 14:14:28 +0100

    Adding elastic autogrowing textareas

--- test_app/public/javascripts/application.js
+++ test_app/public/javascripts/application.js
@@ -3,13 +3,13 @@
 
 $(document).ready(function() {
   /* Activating Best In Place */
-  jQuery(".best_in_place").best_in_place()
+  jQuery(".best_in_place").best_in_place();
 });
 
 function inspect(obj, maxLevels, level)
 {
   var str = '', type, msg;
-
+1
     // Start Input Validations
     // Don't touch, we start iterating at level zero
     if(level == null)  level = 0;
