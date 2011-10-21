25fd40286813d409388f5eada97b080290ac071e
Roger Campos <roger@itnig.net>
2011-10-21 20:05:57 +0200

    Reorganizing specs

--- spec/helpers/best_in_place_spec.rb
+++ spec/helpers/best_in_place_spec.rb
@@ -14,52 +14,65 @@ describe BestInPlace::BestInPlaceHelpers do
         :description => "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus a lectus et lacus ultrices auctor. Morbi aliquet convallis tincidunt. Praesent enim libero, iaculis at commodo nec, fermentum a dolor. Quisque eget eros id felis lacinia faucibus feugiat et ante. Aenean justo nisi, aliquam vel egestas vel, porta in ligula. Etiam molestie, lacus eget tincidunt accumsan, elit justo rhoncus urna, nec pretium neque mi et lorem. Aliquam posuere, dolor quis pulvinar luctus, felis dolor tincidunt leo, eget pretium orci purus ac nibh. Ut enim sem, suscipit ac elementum vitae, sodales vel sem."
     end
 
+    it "should generate a proper span" do
+      nk = Nokogiri::HTML.parse(helper.best_in_place @user, :name)
+      span = nk.css("span")
+      span.should_not be_empty
+    end
+
     describe "general properties" do
-      before { @nk = Nokogiri::HTML.parse(helper.best_in_place @user, :name) }
-
-      it "should render a proper span" do
-        span = @nk.css("span")
-        span.should_not be_empty
-      end
-
-      it "should render the name as text" do
-        span = @nk.css("span")
-        span.text.should == "Lucia"
+      before do
+        nk = Nokogiri::HTML.parse(helper.best_in_place @user, :name)
+        @span = nk.css("span")
       end
 
       it "should have a proper id" do
-        span = @nk.css("span")
-        span.attribute("id").value.should == "best_in_place_user_name"
-
+        @span.attribute("id").value.should == "best_in_place_user_name"
       end
 
       it "should have the best_in_place class" do
-        span = @nk.css("span")
-        span.attribute("class").value.should == "best_in_place"
+        @span.attribute("class").value.should == "best_in_place"
       end
 
       it "should have the correct data-attribute" do
-        span = @nk.css("span")
-        span.attribute("data-attribute").value.should == "name"
-      end
-
-      it "should have the correct data-type" do
-        span = @nk.css("span")
-        span.attribute("data-type").value.should == "input"
+        @span.attribute("data-attribute").value.should == "name"
       end
 
       it "should have the correct default url" do
         @user.save!
-        @nk = Nokogiri::HTML.parse(helper.best_in_place @user, :name)
-        span = @nk.css("span")
+        nk = Nokogiri::HTML.parse(helper.best_in_place @user, :name)
+        span = nk.css("span")
         span.attribute("data-url").value.should == "/users/#{@user.id}"
       end
 
       it "should have the correct data-object" do
-        span = @nk.css("span")
-        span.attribute("data-object").value.should == "user"
+        @span.attribute("data-object").value.should == "user"
+      end
+    end
+
+
+    context "with a text field attribute" do
+      before do
+        nk = Nokogiri::HTML.parse(helper.best_in_place @user, :name)
+        @span = nk.css("span")
       end
 
+      it "should render the name as text" do
+        @span.text.should == "Lucia"
+      end
+
+      it "should have an input data-type" do
+        @span.attribute("data-type").value.should == "input"
+      end
     end
+
+    context "with a boolean attribute" do
+
+    end
+
+    context "with a select-list attribute" do
+
+    end
+
   end
 end
