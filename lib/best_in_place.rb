00270535df3cdcf5b0359198494f3b6f327a9ac9
Albert Bellonch <albert@itnig.net>
2012-01-10 01:31:22 +0100

    Solving issue; the module now is an instance of ActionView::Base

--- lib/best_in_place.rb
+++ lib/best_in_place.rb
@@ -3,11 +3,10 @@ require "best_in_place/helper"
 require "best_in_place/engine"
 require "best_in_place/controller_extensions"
 require "best_in_place/display_methods"
+require "action_view"
 
 module BestInPlace
   autoload :TestHelpers, "best_in_place/test_helpers"
 
-  module ViewHelpers
-    extend ActionView::Helpers
-  end
+  ViewHelpers = ActionView::Base.new
 end
