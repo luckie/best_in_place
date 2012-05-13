3e45f095f6cb3aaec38332a25ad2b5305846f136
Roger Campos <roger@itnig.net>
2012-05-13 03:55:57 +0200

    Escape the collection data attribute, fixes #138

--- lib/best_in_place/helper.rb
+++ lib/best_in_place/helper.rb
@@ -34,7 +34,7 @@ module BestInPlace
       out << " id='#{BestInPlace::Utils.build_best_in_place_id(object, field)}'"
       out << " data-url='#{opts[:path].blank? ? url_for(object) : url_for(opts[:path])}'"
       out << " data-object='#{opts[:object_name] || BestInPlace::Utils.object_to_key(object)}'"
-      out << " data-collection='#{collection.gsub(/'/, "&#39;")}'" unless collection.blank?
+      out << " data-collection='#{escape_javascript(collection)}'" unless collection.blank?
       out << " data-attribute='#{field}'"
       out << " data-activator='#{opts[:activator]}'" unless opts[:activator].blank?
       out << " data-ok-button='#{opts[:ok_button]}'" unless opts[:ok_button].blank?
