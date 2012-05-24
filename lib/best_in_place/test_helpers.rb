cde371c834be0556cc203f026efed2d9736a8bfd
Roger Campos <roger@itnig.net>
2012-05-24 18:28:12 +0200

    compatibility fix: Testing helpers uses jQuery and not $

--- lib/best_in_place/test_helpers.rb
+++ lib/best_in_place/test_helpers.rb
@@ -4,34 +4,34 @@ module BestInPlace
     def bip_area(model, attr, new_value)
       id = BestInPlace::Utils.build_best_in_place_id model, attr
       page.execute_script <<-JS
-        $("##{id}").click();
-        $("##{id} form textarea").val('#{new_value}');
-        $("##{id} form textarea").blur();
+        jQuery("##{id}").click();
+        jQuery("##{id} form textarea").val('#{new_value}');
+        jQuery("##{id} form textarea").blur();
       JS
     end
 
     def bip_text(model, attr, new_value)
       id = BestInPlace::Utils.build_best_in_place_id model, attr
       page.execute_script <<-JS
-        $("##{id}").click();
-        $("##{id} input[name='#{attr}']").val('#{new_value}');
-        $("##{id} form").submit();
+        jQuery("##{id}").click();
+        jQuery("##{id} input[name='#{attr}']").val('#{new_value}');
+        jQuery("##{id} form").submit();
       JS
     end
 
     def bip_bool(model, attr)
       id = BestInPlace::Utils.build_best_in_place_id model, attr
-      page.execute_script("$('##{id}').click();")
+      page.execute_script("jQuery('##{id}').click();")
     end
 
     def bip_select(model, attr, name)
       id = BestInPlace::Utils.build_best_in_place_id model, attr
       page.execute_script <<-JS
         (function() {
-          $("##{id}").click();
-          var opt_value = $("##{id} select option:contains('#{name}')").attr('value');
-          $("##{id} select option[value='" + opt_value + "']").attr('selected', true);
-          $("##{id} select").change();
+          jQuery("##{id}").click();
+          var opt_value = jQuery("##{id} select option:contains('#{name}')").attr('value');
+          jQuery("##{id} select option[value='" + opt_value + "']").attr('selected', true);
+          jQuery("##{id} select").change();
         })();
       JS
     end
